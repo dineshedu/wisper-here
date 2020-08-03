ea5d50ccc0d8ec281cc4bb98947ed84a634e4ab6
dineshsuryanand <dineshsuryanand@gmail.com>
2020-08-03 10:53:07 +0530

    added facebook authnenticatin

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -76,9 +77,23 @@ passport.use(
   )
 );
 
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/whisper"
+},
+  function (accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get("/", function (req, res) {
   res.render("home");
 });
+
+
 
 app.get(
   "/auth/google",
@@ -93,6 +108,17 @@ app.get(
     res.redirect("/secrets");
   }
 );
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/whisper',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 app.get("/login", function (req, res) {
   res.render("login");
@@ -148,5 +174,5 @@ app.post("/login", function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log("server started at port");
+  console.log("server started at port 3000");
 });
